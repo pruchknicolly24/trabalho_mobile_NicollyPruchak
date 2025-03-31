e7feeb79f1c6248e702bc5a37e85cf27839fb1f4
pruchknicolly24 <nicolly.livramento@escola.pr.gov.br>
2025-03-31 18:22:51 -0300

    Atualizando _layout.tsx

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,3 +1,4 @@
+// Teste de alteração para forçar commit
 import React, { useState, useEffect } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
